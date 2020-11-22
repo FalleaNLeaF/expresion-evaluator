0ee276696f1e4e27b0155ccbe7353b2bd773116b
FalleaNLeaF <74867299+FalleaNLeaF@users.noreply.github.com>
2020-11-22 17:18:08 +0200

    Update evaluator.c

--- evaluator.c
+++ evaluator.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
-char expr[20];//array to store entered expression
+char expr[20];//array to store entered expression                                //почему именно 20? если правильно поняла, чтобы компиляция кода много времени не занимала?
 char stack[20];//store the postfix expression
 int precedence(char a, char b) {//returns true if precedence of operator a is more or equal to than that of b
-    return ((a == '+') || (a == '-')) && ((b == '*') || (b == '/')) ? 0 : 1;
+    return ((a == '+') || (a == '-')) && ((b == '*') || (b == '/')) ? 0 : 1;     // что здесь значит знак вопроса перед нулем?
 }
 
 int i;
 int ctr;
-int topOfValueStack = -1;
-int topOperatorStack = -1;
+int topOfValueStack = -1;                                                       // почему присваивается значение -1
+int topOperatorStack = -1;                                                      // почему присваивается значение -1
 
 int applyOperator(int a, int b, char oper) {
     switch (oper) {
@@ -29,28 +29,28 @@ int applyOperator(int a, int b, char oper) {
 
 void postfixConvert() {
     char topsymb, operatorStack[20];
-    ctr = 0;
+    ctr = 0;                                                                    // что значит "ctr"
     while (expr[ctr] != '\0') {
         if (expr[ctr] >= '0' && expr[ctr] <= '9') {
             int val = 0, step = 1;
             while (expr[ctr] != '\0' && isdigit(expr[ctr])) {
-                val = val * step + (int) (expr[ctr] - '0');
-                step *= 10;
+                val = val * step + (int) (expr[ctr] - '0');           // это действие  val=val*step+(int), что здесь значит этот (int), и за что отвечает это действие?
+                step *= 10;                                           // это эквивалентно step = step * 10 ?
                 ctr++;
             }
-            stack[++topOfValueStack] = val + '0';
+            stack[++topOfValueStack] = val + '0';                     // эти две строчки отвечают за возврат на предыдущий символ, при условии, что проверка дошла до символа окончания?
             ctr--;
-        } else {
-            while (topOperatorStack >= 0 && precedence(operatorStack[topOperatorStack], expr[ctr])) {
+        } else {                                                      // полностью, что выполняет это else, вообще не понимаю
+            while (topOperatorStack >= 0 && precedence(operatorStack[topOperatorStack], expr[ctr])) {                 
                 topsymb = operatorStack[topOperatorStack--];
-                stack[++topOfValueStack] = topsymb;
+                stack[++topOfValueStack] = topsymb;                   //зачем здесь stack
             }
-            operatorStack[++topOperatorStack] = expr[ctr];
+            operatorStack[++topOperatorStack] = expr[ctr];            // и что выполняет вот эта строка
         }
         ctr++;
     }
     while (topOperatorStack >= 0) {
-        stack[++topOfValueStack] = operatorStack[topOperatorStack--];
+        stack[++topOfValueStack] = operatorStack[topOperatorStack--];  //зачем здесь stack
     }
 }
 
@@ -60,12 +60,12 @@ int main() {
     postfixConvert();
     int operand1, operand2, temp;
     ctr = 0;
-    int result[2];
-    int rTop = -1;
+    int result[2];                                                    // почему именно второе место присваивается?
+    int rTop = -1;                                                    // почему присваивается значение -1? и что именно значит rTop?
     while (stack[ctr] != '\0') {
-        temp = (int) (stack[ctr] - '0');
+        temp = (int) (stack[ctr] - '0');                              // что выполняет это выражение temp = (int) (stack[ctr] - '0');
         if (temp >= 0)
-            result[++rTop] = temp;
+            result[++rTop] = temp;                                    
         else {
             operand1 = result[rTop--];
             operand2 = result[rTop--];
@@ -74,5 +74,5 @@ int main() {
         ctr++;
     }
     printf("The result of the expression is: %d\n", result[0]);
-    getchar();
-}
+    getchar();                                                         // спасибо огромное за помощь и извини за беспокойство :) Даже не знаю, как отблагодарить, если нужна будет от меня какая-то помощь, с удовольствием помогу!
+} 
